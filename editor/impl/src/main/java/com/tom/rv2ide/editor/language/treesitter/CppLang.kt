23276f39aa856b9d2954611a9c35b3535ba02993
Website <haickarpin2998@gmail.com>
2026-03-12 12:47:34 +0530

    Update CppLang.kt

--- editor/impl/src/main/java/com/tom/rv2ide/editor/language/treesitter/CppLang.kt
+++ editor/impl/src/main/java/com/tom/rv2ide/editor/language/treesitter/CppLang.kt
@@ -21,7 +21,7 @@ import android.content.Context
 import com.tom.rv2ide.editor.language.treesitter.TreeSitterLanguage.Factory
 import com.tom.rv2ide.lsp.api.ILanguageServer
 import com.tom.rv2ide.lsp.api.ILanguageServerRegistry
-import com.tom.rv2ide.lsp.clang.ClangLanguageServer
+// import com.tom.rv2ide.lsp.clang.ClangLanguageServer
 import io.github.rosemoe.sora.util.MyCharacter
 import com.tom.rv2ide.treesitter.cpp.TSLanguageCpp
 import io.github.rosemoe.sora.lang.Language.INTERRUPTION_LEVEL_STRONG
@@ -43,7 +43,7 @@ open class CppLang(context: Context) :
     const val TS_TYPE_HPP = "hpp"
   }
 
-  override val languageServer: ILanguageServer?
-    get() = ILanguageServerRegistry.getDefault().getServer(ClangLanguageServer.SERVER_ID)
+ // override val languageServer: ILanguageServer?
+  //  get() = ILanguageServerRegistry.getDefault().getServer(ClangLanguageServer.SERVER_ID)
 
 }
