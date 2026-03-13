759b611dca10e3bf4db62b1790e28687fc3e704c
Neeraj Engineer <haickarpin2998@gmail.com>
2026-03-13 10:24:56 +0530

    Update build.gradle.kts

--- core/app/build.gradle.kts
+++ core/app/build.gradle.kts
@@ -62,10 +62,10 @@ configurations.all {
 }
 
 android {
-  namespace = ("com.androidgit.optiongit")
+  namespace = "com.tom.rv2ide"
 
   defaultConfig {
-    applicationId = ("com.androidgit.optiongit")
+    applicationId = "com.androidgit.optiongit"
     vectorDrawables.useSupportLibrary = true
   }
   
