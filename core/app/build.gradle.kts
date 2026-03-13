c627dbad8902c8ec68fd6d515d890e00f390e59a
Neeraj Engineer <haickarpin2998@gmail.com>
2026-03-13 10:45:47 +0530

    Update build.gradle.kts

--- core/app/build.gradle.kts
+++ core/app/build.gradle.kts
@@ -17,7 +17,7 @@
 
 @file:Suppress("UnstableApiUsage")
 
-import com.tom.rv2ide.build.config.BuildConfig
+import com.tom.rv2idem.build.config.BuildConfig
 import com.tom.rv2ide.desugaring.utils.JavaIOReplacements.applyJavaIOReplacements
 import com.tom.rv2ide.plugins.AndroidIDEAssetsPlugin
 import java.util.Properties
@@ -62,10 +62,10 @@ configurations.all {
 }
 
 android {
-  namespace = "com.tom.rv2ide"
+  namespace = BuildConfig.packageName
 
   defaultConfig {
-    applicationId = "com.androidgit.optiongit"
+    applicationId = BuildConfig.packageName
     vectorDrawables.useSupportLibrary = true
   }
   
