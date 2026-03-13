407b93976aeffc2d7c957a3da491fdc90cfacbd7
Neeraj Engineer <haickarpin2998@gmail.com>
2026-03-13 10:07:06 +0530

    Update NonEditableEditorFragment.java

--- core/app/src/main/java/com/tom/rv2ide/fragments/output/NonEditableEditorFragment.java
+++ core/app/src/main/java/com/tom/rv2ide/fragments/output/NonEditableEditorFragment.java
@@ -22,7 +22,7 @@ import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.tom.rv2ide.R;
-import com.tom.rv2ide.databinding.FragmentNonEditableEditorBinding;
+import com.androidgit.optiongit.databinding.FragmentNonEditableEditorBinding;
 import com.tom.rv2ide.editor.ui.IDEEditor;
 import com.tom.rv2ide.fragments.EmptyStateFragment;
 import com.tom.rv2ide.syntax.colorschemes.SchemeAndroidIDE;
@@ -95,3 +95,4 @@ public abstract class NonEditableEditorFragment extends
     getEmptyStateViewModel().isEmpty().setValue(true);
   }
 }
+
